ce50fd36d047da59af65506076590c149b5d0691
Dina Omar <dina.omar@momkn.com.eg>
2020-02-01 18:07:59 +0200

    sound synch with text

--- app/src/main/java/com/dina/elcg/alnedaa/fragments/GameFragment.kt
+++ app/src/main/java/com/dina/elcg/alnedaa/fragments/GameFragment.kt
@@ -1,17 +1,38 @@
 package com.dina.elcg.alnedaa.fragments
 
 import android.content.Context
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.BackgroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dina.elcg.alnedaa.R
+import kotlinx.android.synthetic.main.fragment_game.*
+import java.util.*
 
 
 class GameFragment : Fragment() {
 
+    var mPlayerbackground: MediaPlayer? = null
+    var mPlayerQustion: MediaPlayer? = null
+    lateinit var handlerOne: Handler
+    lateinit var handlerTwo: Handler
+    lateinit var handlerThree: Handler
+
+    var i = 0
+    var start = 0
+    lateinit var stringOne: String
+    lateinit var wordsOne: List<String>
+    lateinit var stringTwo: String
+    lateinit var wordsTwo: List<String>
+    lateinit var stringThree: String
+    lateinit var wordsThree: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +49,38 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mPlayer: MediaPlayer = MediaPlayer.create(this.context, R.raw.musicbg)
-        mPlayer.setVolume(50f,50f)
-        mPlayer.start()
+        mPlayerbackground = MediaPlayer.create(this.context, R.raw.musicbg)
+        mPlayerQustion = MediaPlayer.create(this.context, R.raw.vo)
+        mPlayerbackground?.setVolume(0.3f, 0.3f)
+        stringOne = textView1.text.toString()
+        wordsOne = stringOne.split(" ")
+
+        stringTwo = textView2.text.toString()
+        wordsTwo = stringTwo.split(" ")
+
+        stringThree = textView3.text.toString()
+        wordsThree = stringThree.split(" ")
+
+        // Initialize the handler instance
+        handlerOne = Handler()
+        handlerThree = Handler()
+        handlerTwo = Handler()
+
+        mPlayerbackground?.start()
+        startTimer()
+
+    }
+
+    private fun startTimer() {
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                mPlayerbackground?.setVolume(0.1f, 0.1f)
+                mPlayerQustion?.start()
+                handlerOne.postDelayed(runnableOne, 1000)
+
+            }
+        }, 1000)
     }
 
 
@@ -41,7 +91,95 @@ class GameFragment : Fragment() {
 
     override fun onDetach() {
         super.onDetach()
-//        mPlayer.stop()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mPlayerbackground?.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mPlayerbackground = null
+    }
+
+    private val runnableOne = object : Runnable {
+        override fun run() {
+            var span = SpannableStringBuilder()
+
+            if (i < wordsOne.size) {
+                span = SpannableStringBuilder(stringOne)
+                span.setSpan(
+                    BackgroundColorSpan(Color.YELLOW),
+                    start,
+                    wordsOne[i].length + start,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                textView1.setTextKeepState(span)
+                start += wordsOne[i].length + i
+                i++
+                span.clearSpans()
+                handlerOne.postDelayed(this, 1000)
+
+            } else {
+                i = 0
+                start = 0
+                span.clearSpans()
+                handlerTwo.postDelayed(runnableTwo, 100)
+            }
+        }
+
+    }
+
+    private val runnableTwo = object : Runnable {
+        override fun run() {
+            var span = SpannableStringBuilder()
+            if (i < wordsTwo.size) {
+                span = SpannableStringBuilder(stringTwo)
+                span.setSpan(
+                    BackgroundColorSpan(Color.YELLOW),
+                    start,
+                    wordsTwo[i].length + start,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                textView2.setTextKeepState(span)
+                start += wordsTwo[i].length + i
+                i++
+                span.clearSpans()
+                handlerTwo.postDelayed(this, 800)
+
+            } else {
+                i = 0
+                start = 0
+                handlerThree.postDelayed(runnableThree, 10)
+            }
+        }
+    }
+
+    private val runnableThree = object : Runnable {
+        override fun run() {
+            var span = SpannableStringBuilder()
+            if (i < wordsThree.size) {
+                span = SpannableStringBuilder(stringThree)
+                span.setSpan(
+                    BackgroundColorSpan(Color.YELLOW),
+                    start,
+                    wordsThree[i].length + start,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                textView3.setTextKeepState(span)
+                start += wordsThree[i].length + i
+                i++
+                span.clearSpans()
+                handlerThree.postDelayed(this, 700)
+
+            } else {
+                i = 0
+                start = 0
+                span.clear()
+            }
+
+        }
     }
 
 
