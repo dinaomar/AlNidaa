3c4c7fb5a43384f9ded63c6d42c9ca3c1ccf735d
Dina Omar <dina.omar@momkn.com.eg>
2020-02-05 14:04:33 +0200

    drop issue not solved

--- app/src/main/java/com/dina/elcg/alnedaa/fragments/QuestionsFragment.kt
+++ app/src/main/java/com/dina/elcg/alnedaa/fragments/QuestionsFragment.kt
@@ -15,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.dina.elcg.alnedaa.R
+import com.dina.elcg.alnedaa.getLocationOnScreen
 import kotlinx.android.synthetic.main.fragment_questions.*
 
 
@@ -42,6 +43,10 @@ class QuestionsFragment : Fragment() {
         mPlayerbackground?.setVolume(0.3f, 0.3f)
         mPlayerbackground?.start()
 
+        getXY(line1)
+        getXY(line2)
+        getXY(line3)
+
         upBt.setOnClickListener { functionUp() }
         downBt.setOnClickListener { functionDown() }
         leftBt.setOnClickListener { functionLeft() }
@@ -53,7 +58,7 @@ class QuestionsFragment : Fragment() {
     @SuppressLint("ResourceType")
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private fun functionSelect() {
-        if (cursorLayout.childCount == 1) {
+        if (cursorLayout.childCount == 0) {
             when {
                 checkCollision(cursorLayout, wordOne) -> {
                     wordOne.backgroundTintList =
@@ -101,89 +106,100 @@ class QuestionsFragment : Fragment() {
         } else {
 
             when {
-                checkCollision(cursorLayout[1], lineOne) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
+                checkCollision(cursorLayout[0], lines[0]) -> {
+                    val view = cursorLayout[0]
                     if (view.parent != null)
                         (view.parent as ViewGroup).removeView(view)
-                    lineOne.addView(view)
-                }
-                checkCollision(cursorLayout[1], lineTwo) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
-                    if (view.parent != null)
-                        (view.parent as ViewGroup).removeView(view)
-                    lineTwo.addView(view)
-                }
-                checkCollision(cursorLayout[1], lineThree) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
-                    if (view.parent != null)
-                        (view.parent as ViewGroup).removeView(view)
-                    lineThree.addView(view)
-                }
-                checkCollision(cursorLayout[1], lineFour) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
-                    if (view.parent != null)
-                        (view.parent as ViewGroup).removeView(view)
-                    lineFour.addView(view)
-                }
-                checkCollision(cursorLayout[1], lineFive) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
-                    if (view.parent != null)
-                        (view.parent as ViewGroup).removeView(view)
-                    lineFive.addView(view)
-                }
-                checkCollision(cursorLayout[1], lineSix) -> {
-                    val view: View = cursorLayout.getChildAt(1)
-                    cursorLayout.removeViewAt(1)
-                    if (view.parent != null)
-                        (view.parent as ViewGroup).removeView(view)
-                    lineSix.addView(view)
+                    targets.addView(view)
                     checkResult()
                 }
             }
 
         }
-
     }
 
+
     private fun checkResult() {
-        if (lineSix.tag == lineSix.getChildAt(1).tag){
-            Log.w("tag","correct")
+        if (targets.childCount == 6) {
+            if (targets[0].tag == "one" && targets[1].tag == "two" &&
+                targets[2].tag == "three" && targets[3].tag == "four" &&
+                targets[4].tag == "five" && targets[5].tag == "six"
+            )
+                Log.w("right", "done")
         }
     }
 
     private fun checkCollision(v1: View, v2: View): Boolean {
-        val R1 = Rect(v1.left, v1.top, v1.right, v1.bottom)
-        val R2 = Rect(v2.left, v2.top, v2.right, v2.bottom)
-        return R1.intersect(R2)
+        var rect1 = Rect()
+
+        v1.getHitRect(rect1)
+        var rect2 = Rect()
+        v2.getHitRect(rect2)
+
+//        val R1 = Rect(v1.left, v1.top, v1.right, v1.bottom)
+//        val R2 = Rect(v2.left, v2.top, v2.right, v2.bottom)
+//        return R1.intersect(R2)
+
+        return rect1.intersect(rect2)
+    }
+
+    private fun viewsOverlap(v1: View, v2: View): Boolean {
+        val v1_coords = IntArray(2)
+        v1.getLocationInWindow(v1_coords)
+        val v1_w = v1.width
+        val v1_h = v1.height
+        val v1_rect = Rect(
+            v1_coords[0],
+            v1_coords[1],
+            v1_coords[0] + v1_w,
+            v1_coords[1] + v1_h
+        )
+        val v2_coords = IntArray(2)
+        v2.getLocationInWindow(v1_coords)
+        val v2_w = v2.width
+        val v2_h = v2.height
+        val v2_rect = Rect(
+            v2_coords[0],
+            v2_coords[1],
+            v2_coords[0] + v2_w,
+            v2_coords[1] + v2_h
+        )
+        return v1_rect.intersect(v2_rect) || v1_rect.contains(v2_rect) || v2_rect.contains(v1_rect)
     }
 
     private fun functionUp() {
         val paramsTop = cursorLayout.layoutParams as RelativeLayout.LayoutParams
-        paramsTop.bottomMargin += 20
+        paramsTop.bottomMargin += 60
         cursorLayout.layoutParams = paramsTop
+        cursorLayout.invalidate()
+        getXY(cursorLayout)
     }
 
     private fun functionDown() {
         val paramsBottom = cursorLayout.layoutParams as RelativeLayout.LayoutParams
-        paramsBottom.bottomMargin -= 20
+        paramsBottom.bottomMargin -= 60
         cursorLayout.layoutParams = paramsBottom
+        cursorLayout.invalidate()
     }
 
     private fun functionLeft() {
         val paramsLeft = cursorLayout.layoutParams as RelativeLayout.LayoutParams
-        paramsLeft.leftMargin -= 20
+        paramsLeft.leftMargin -= 60
         cursorLayout.layoutParams = paramsLeft
+        cursorLayout.invalidate()
     }
 
     private fun functionRight() {
         val paramsRight = cursorLayout.layoutParams as RelativeLayout.LayoutParams
-        paramsRight.leftMargin += 20
+        paramsRight.leftMargin += 60
         cursorLayout.layoutParams = paramsRight
+        cursorLayout.invalidate()
+    }
+
+    fun getXY(view: View) {
+        Log.w("dinax", "" + view.getLocationOnScreen().x)
+        Log.w("dinay", "" + view.getLocationOnScreen().y)
+
     }
 
 
