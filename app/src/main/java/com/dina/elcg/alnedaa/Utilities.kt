500ef54fb633ffb429975db133ea9c90cf2d7f7e
dina <dina.elcg@gmail.com>
2020-02-16 11:43:41 -0800

    dynamic layout

---
+++ app/src/main/java/com/dina/elcg/alnedaa/Utilities.kt
@@ -0,0 +1,8 @@
+package com.dina.elcg.alnedaa
+
+object Utilities {
+
+    fun splitQuestion(question: String): List<String> {
+        return question.split("\\s".toRegex())
+    }
+}
--- app/src/main/java/com/dina/elcg/alnedaa/fragments/QuestionsFragment.kt
+++ app/src/main/java/com/dina/elcg/alnedaa/fragments/QuestionsFragment.kt
@@ -11,11 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import com.dina.elcg.alnedaa.QuestionsBank
 import com.dina.elcg.alnedaa.R
+import com.dina.elcg.alnedaa.Utilities
 import com.dina.elcg.alnedaa.getLocationOnScreen
 import kotlinx.android.synthetic.main.fragment_questions.*
 
@@ -36,19 +39,36 @@ class QuestionsFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mPlayerbackground = MediaPlayer.create(this.context, R.raw.musicbg)
-        mPlayerbackground?.setVolume(0.1f, 0.1f)
-        mPlayerbackground?.start()
+//        mPlayerbackground = MediaPlayer.create(this.context, R.raw.musicbg)
+//        mPlayerbackground?.setVolume(0.1f, 0.1f)
+//        mPlayerbackground?.start()
 
-        getXY(line1)
-        getXY(line2)
-        getXY(line3)
 
         upBt.setOnClickListener { functionUp() }
         downBt.setOnClickListener { functionDown() }
         leftBt.setOnClickListener { functionLeft() }
         rightBt.setOnClickListener { functionRight() }
         selectBtn.setOnClickListener { functionSelect() }
+
+        drawLayout()
+    }
+
+    private fun drawLayout() {
+        val words: List<String> = Utilities.splitQuestion(QuestionsBank.questionOne)
+        for (word: String in words) {
+            val wordContainer = RelativeLayout(requireContext())
+            val wordText = TextView(requireContext())
+            val imageBorder = ImageView(requireContext())
+            imageBorder.setImageResource(R.drawable.choicebox)
+            imageBorder.layoutParams.width = 80
+            imageBorder.layoutParams.height = 50
+            wordText.text = word
+            wordContainer.addView(imageBorder)
+            wordContainer.addView(wordText)
+            wordsLayout.addView(wordContainer)
+            val imageLine = ImageView(requireContext())
+            lines.addView(imageLine)
+        }
     }
 
 
@@ -56,92 +76,8 @@ class QuestionsFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private fun functionSelect() {
 //        if (cursorLayout.childCount == 1) {
-            when {
-                checkCollision(cursor, wordOne) -> {
-                    wordOne.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordOne.parent != null)
-                        (wordOne.parent as ViewGroup).removeView(wordOne)
-                    cursorLayout.addView(wordOne)
-                }
-                checkCollision(cursor, wordTwo) -> {
-                    wordTwo.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordTwo.parent != null)
-                        (wordTwo.parent as ViewGroup).removeView(wordTwo)
-                    cursorLayout.addView(wordTwo)
-                }
-                checkCollision(cursor, wordThree) -> {
-                    wordThree.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordThree.parent != null)
-                        (wordThree.parent as ViewGroup).removeView(wordThree)
-                    cursorLayout.addView(wordThree)
+        when {
 
-                }
-                checkCollision(cursor, wordFour) -> {
-                    wordFour.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordFour.parent != null)
-                        (wordFour.parent as ViewGroup).removeView(wordFour)
-                    cursorLayout.addView(wordFour)
-                }
-                checkCollision(cursor, wordFive) -> {
-                    wordFive.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordFive.parent != null)
-                        (wordFive.parent as ViewGroup).removeView(wordFive)
-                    cursorLayout.addView(wordFive)
-                }
-                checkCollision(cursor, wordSix) -> {
-                    wordSix.backgroundTintList =
-                        ContextCompat.getColorStateList(requireContext(), R.color.terquaz)
-                    if (wordSix.parent != null)
-                        (wordSix.parent as ViewGroup).removeView(wordSix)
-                    cursorLayout.addView(wordSix)
-                }
-                checkCollision(wordOne, lines) -> {
-                    if (wordOne.parent != null)
-                        (wordOne.parent as ViewGroup).removeView(wordOne)
-                    targets.addView(wordOne)
-                    cursorLayout.removeView(wordOne)
-                    checkResult()
-                }
-                checkCollision(wordTwo, lines) -> {
-                    if (wordTwo.parent != null)
-                        (wordTwo.parent as ViewGroup).removeView(wordTwo)
-                    targets.addView(wordTwo)
-                    cursorLayout.removeView(wordTwo)
-                    checkResult()
-                }
-                checkCollision(wordThree, lines[2]) -> {
-                    if (wordThree.parent != null)
-                        (wordThree.parent as ViewGroup).removeView(wordThree)
-                    targets.addView(wordThree)
-                    cursorLayout.removeView(wordThree)
-                    checkResult()
-                }
-                checkCollision(wordFour, lines[3]) -> {
-                    if (wordFour.parent != null)
-                        (wordFour.parent as ViewGroup).removeView(wordFour)
-                    targets.addView(wordFour)
-                    cursorLayout.removeView(wordFour)
-                    checkResult()
-                }
-                checkCollision(wordFive, lines[4]) -> {
-                    if (wordFive.parent != null)
-                        (wordFive.parent as ViewGroup).removeView(wordFive)
-                    targets.addView(wordFive)
-                    cursorLayout.removeView(wordFive)
-                    checkResult()
-                }
-                checkCollision(wordSix, lines[5]) -> {
-                    if (wordSix.parent != null)
-                        (wordSix.parent as ViewGroup).removeView(wordSix)
-                    targets.addView(wordSix)
-                    cursorLayout.removeView(wordSix)
-                    checkResult()
-                }
 //            }
 //        } else {
 //
